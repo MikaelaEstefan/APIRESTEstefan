96341919055557d7d293d1e6ab40678de8aa689d
Mikaela Estefan <estefanmikaela@gmail.com>
2023-11-08 23:20:02 -0300

    first commit

---
+++ src/app/dashboard/pages/users/components/users-table/users-table.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../../models';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-users-table',
+  templateUrl: './users-table.component.html',
+  styles: [],
+})
+export class UsersTableComponent {
+  @Input()
+  dataSource: User[] = [];
+
+  @Output()
+  deleteUser = new EventEmitter<number>();
+
+  @Output()
+  editUser = new EventEmitter<User>();
+
+  displayedColumns = ['id', 'fullname', 'email', 'actions'];
+
+  constructor(private router: Router) {}
+
+  goToDetail(userId: number): void {
+    this.router.navigate(
+      [
+        'dashboard',
+        'users',
+        'detail',
+        userId,
+        {
+          nombre: 'Pippin',
+          edad: 18,
+        },
+      ],
+      {
+        queryParams: {
+          search: 'Fool of a took',
+        },
+      }
+    );
+  }
+}
